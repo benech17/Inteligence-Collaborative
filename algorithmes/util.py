ac5924c5097ccb49c2ef204fc9c94bf5cf16f8eb
Yohann Le couster <ylecoust@domaine.lan>
2022-04-04 09:40:29 +0200

    résolution conflit util

--- algorithmes/util.py
+++ algorithmes/util.py
@@ -1,4 +1,6 @@
 import csv
+import random as rd
+from math import sin, cos, sqrt, atan2, radians, exp
 
 class Depot:
     def __init__(self, dept_code, dpt_lat, dpt_long):
@@ -18,19 +20,56 @@ class Client:
         self.number_of_articles = num_of_art
         self.total_weight_kg = wght_kg
         self.total_volume_m3 = vol_m3
+    
+    def calc_dist(self, Client client):
+        k=6373.0 #rayon de la terre
+        d_long = radians(client.customer_longitude) - radians(self.customer_longitude)
+        d_lat = radians(client.customer_latitude) - radians(self.customer_longitude)
+        x = sin(d_lat / 2)**2 + cos(radians(lat_a)) * cos(radians(lat_b)) * sin(d_long / 2)**2
+        z = 2 * atan2(sqrt(x), sqrt(1 - x))
+        return(k*z)
 
 class Vehicules:
-    def __init__(self, vh_cd, vh_total_wght, vh_total_vol, vh_fx_cost_km, vh_vr_cost_time,vh_speed):
+    def __init__(self, vh_cd, vh_total_wght, vh_total_vol, vh_fx_cost_km, vh_vr_cost_time):
         self.vehicle_code = vh_cd
         self.vehicle_total_weight = vh_total_wght
         self.vehicle_total_volume = vh_total_vol
+        self.vehicle_weight = 0
+        self.vehicle_volume = 0
         self.vehicle_fixed_cost_km = vh_fx_cost_km
         self.vehicle_variable_cost_km = vh_vr_cost_time
-        self.vehicle_speed = vh_speed
+    
+    def add_client_order(self, Client client):
+        self.vehicle_weight += client.total_weight_kg
+        self.vehicle_volume += client.total_volume_m3
+        if (self.vehicle_weight > self.vehicle_total_weight) or (self.vehicle_volume > self.vehicle_total_volume):
+            return(True)
+        return(False)
 
-class Routes:
+class Liste_Clients:
     def __init__(self, unique_id):
         self.id = unique_id
+        self.liste = []
+    
+    def shuffle_list(self):
+        self.liste = rd.shuffle(self.liste)
+
+    def permutation_list(self):
+        n=len(self.list)
+        i=rd.randint(0,n-1)
+        j=rd.randint(0,n-1)
+        while(i==j):
+            j=rd.randint(1,n-2)
+        x=self.list[i]
+        self.list[i]=self.list[j]
+        self.list[j]=x
+
+    #Avant de muter une liste, on créé une nouvelle instance puis on copie le contenu de la liste à muter dans la seconde
+    def copy_list(self, Liste_Clients liste_clients):
+        self.liste = liste_clients.liste.copy()
+
+    def add_client_to_list(self, Client client):
+        self.liste.append(client)
 
 def read_files():
     file = open("2_detail_table_customers.csv")
